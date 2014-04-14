f6cca77017bb81c8ba59a2fffd32ee27ec29fd7a
cjgammon <cj.gammon@gmail.com>
2014-04-14 16:02:35 -0700

    add more animations

---
+++ src/js/app/views/cells/frame56.js
@@ -0,0 +1,38 @@
+/*global define $ requestAnimationFrame*/
+
+define(function (require) {
+	
+	var Backbone = require('backbone'),
+        Vars = require('app/models/vars'),
+        UserEvent = require('app/events/user-event'),
+        AppEvent = require('app/events/app-event'),
+        CellView = require('app/views/cells/cell-view'),
+        Frame;
+
+    Frame = CellView.extend({
+        initialize: function () {
+            CellView.prototype.initialize.call(this);
+        },
+
+        render: function (ctx) {
+            var currentFrame = this.options.num == Vars.get('currentFrame'),
+                i;
+
+            if (currentFrame && this.layers.length > 0) {
+                this.layers[0].x = this.layers[0].x > this.layers[0].origin.x - 30 ? this.layers[0].x - 0.5 : this.layers[0].origin.x - 30;
+
+            } else {
+                for (i = 0; i < this.layers.length; i += 1) {
+                    this.layers[i].x = this.layers[i].origin.x;
+                    this.layers[i].y = this.layers[i].origin.y;
+                    this.layers[i].rotation = 0;
+                }
+            }
+
+            CellView.prototype.render.call(this, ctx);
+        }
+    });
+
+	return Frame;
+});
+
---
+++ src/js/app/views/cells/frame70.js
@@ -0,0 +1,43 @@
+/*global define $ requestAnimationFrame*/
+
+define(function (require) {
+	
+	var Backbone = require('backbone'),
+        Vars = require('app/models/vars'),
+        UserEvent = require('app/events/user-event'),
+        AppEvent = require('app/events/app-event'),
+        CellView = require('app/views/cells/cell-view'),
+        Frame;
+
+    Frame = CellView.extend({
+        initialize: function () {
+            CellView.prototype.initialize.call(this);
+        },
+
+        render: function (ctx) {
+            var currentFrame = this.options.num == Vars.get('currentFrame'),
+                i;
+
+            if (currentFrame && this.layers.length > 0) {
+                this.delta += 0.1;
+                this.layers[0].y = this.layers[0].y < this.layers[0].origin.y + 250 ? this.layers[0].y + 5 : this.layers[0].origin.y + 250;
+                this.layers[1].y = this.layers[1].y > this.layers[1].origin.y - 30 ? this.layers[1].y - 0.5 : this.layers[1].origin.y - 30;
+                this.layers[2].y = this.layers[2].y < this.layers[2].origin.y + 30 ? this.layers[2].y + 0.5 : this.layers[2].origin.y + 30;
+                this.layers[3].y = this.layers[3].y < this.layers[3].origin.y + 30 ? this.layers[3].y + 0.5 : this.layers[3].origin.y + 30;
+
+            } else {
+                this.delta = 0;
+                for (i = 0; i < this.layers.length; i += 1) {
+                    this.layers[i].x = this.layers[i].origin.x;
+                    this.layers[i].y = this.layers[i].origin.y;
+                    this.layers[i].rotation = 0;
+                }
+            }
+
+            CellView.prototype.render.call(this, ctx);
+        }
+    });
+
+	return Frame;
+});
+
